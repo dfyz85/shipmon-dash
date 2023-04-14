a6f0d22e916462f650e33a7c9c1c41c6e090f4b1
Ivan <ivan@BrainBook-Pro.local>
2023-04-14 09:56:35 +0300

    Add WSGI script

--- sandbox.py
+++ sandbox.py
@@ -254,4 +254,4 @@ def display_page(n,data):
         raise PreventUpdate
     
 if __name__ == "__main__":
-    app.run_server(debug=True,port=8080,host="0.0.0.0")
+    app.run_server(debug=False,port=8080,host="0.0.0.0")
